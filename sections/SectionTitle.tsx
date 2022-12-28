85d4298566b66eaf5cfd1da1ca92d4a61a4390c9
Bruno Rodrigues <brunorodriguesricardo@gmail.com>
2022-12-27 23:32:21 -0300

    fix flex column

--- sections/SectionTitle.tsx
+++ sections/SectionTitle.tsx
@@ -5,12 +5,14 @@ export interface Props {
 
 export default function SectionTitle({ title, subTitle }: Props) {
   return (
-    <div class="w-full max-w-screen-2xl px-7 mx-auto my-20 lg:my-[7.5rem] px-6">
+    <div class="w-full max-w-screen-2xl px-7 mx-auto my-20 lg:my-[7.5rem] px-6 flex flex-col">
       <h2 class="font-bold w-full text-center md:my-5 text-[7.5vw] lg:text-[2rem]">
         {title}
       </h2>
       {subTitle && (
-        <p class="font-medium text-lg text-center mt-7 lg:max-w-[37%]">{subTitle}</p>
+        <p class="font-medium text-lg text-center mt-7 my-auto lg:max-w-[37%]">
+          {subTitle}
+        </p>
       )}
     </div>
   );
